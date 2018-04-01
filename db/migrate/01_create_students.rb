aef020eaf1a0a6e0a7cbd05cd6f849b920925259
Jawann <jawann@gmail.com>
2018-04-01 22:26:11 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,20 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def up
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY
+      )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def down
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
